7e49aa1755c7f06d4cff27083486afd7a3a68c84
isaias sosa <isosa@castor.com>
2024-08-30 17:24:50 -0300

    prueba

--- js/detalle.js
+++ js/detalle.js
@@ -3,17 +3,17 @@ const urlParams = new URLSearchParams(window.location.search);
         const destinos = {
             'playa-tropical': {
                 titulo: 'Playa Tropical',
-                imagen: './img/saopablo.jpg',
+                imagen: 'img/saopablo.jpg',
                 descripcion: 'Disfruta de unas vacaciones inolvidables en una de las playas más hermosas del mundo. Incluye vuelo ida y vuelta, alojamiento en hotel 5 estrellas, excursiones y actividades acuáticas.'
             },
             'montanas-nevadas': {
                 titulo: 'Montañas Nevadas',
-                imagen: './img/argentina.jpg',
+                imagen: 'img/argentina.jpg',
                 descripcion: 'Experimenta la belleza de las montañas nevadas con este paquete que incluye alojamiento en un resort de lujo, actividades de esquí y transporte desde y hacia el aeropuerto.'
             },
             'ciudad-europea': {
                 titulo: 'Ciudad Europea',
-                imagen: './img/malasia.jpg',
+                imagen: 'img/malasia.jpg',
                 descripcion: 'Explora la rica historia y cultura de una vibrante ciudad europea. El paquete incluye vuelo, alojamiento en un hotel céntrico y tours guiados por la ciudad.'
             }
         };
